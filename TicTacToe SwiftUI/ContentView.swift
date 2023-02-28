0a77e8fac92784a4950d5dde182e6acfe8ab774f
morisoinc <moriso.inc@gmail.com>
2023-02-28 17:44:21 -0300

    added moves, dumb computer with delayed plays

--- TicTacToe SwiftUI/ContentView.swift
+++ TicTacToe SwiftUI/ContentView.swift
@@ -7,13 +7,17 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [
-    GridItem(.flexible()),
-    GridItem(.flexible()),
-    GridItem(.flexible()),
-]
-
 struct ContentView: View {
+
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -22,14 +26,30 @@ struct ContentView: View {
                     ForEach(0..<9) { i in
                         ZStack {
                             Circle()
-                                .foregroundColor(.teal).opacity(0.5)
+                                .foregroundColor(.teal).opacity(
+                                    moves[i] == nil ? 0.3 : 0.7
+                                )
                                 .frame(
                                     width: geometry.size.width / 3 - 16
                                 )
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) || !isHumansTurn { return }
+                            moves[i] = Move(player: .human, boardIndex: i)
+                            
+                            isHumansTurn.toggle()
+                            
+                            if isBoardFilled(in: moves) { return }
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isHumansTurn.toggle()
+                            }
                         }
                     }
                 }
@@ -37,7 +57,38 @@ struct ContentView: View {
                 Spacer()
             }
         }
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: {$0?.boardIndex == index})
+    }
+    
+    func isBoardFilled(in moves: [Move?]) -> Bool {
+        return moves.allSatisfy({$0 != nil})
+    }
+    
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
         
+        while isSquareOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        
+        return movePosition
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
